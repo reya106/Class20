70834a5c24fb413af63bb7bbf79aec587d2736bc
reya106 <75875442+reya106@users.noreply.github.com>
2021-01-15 15:58:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var fixed,moving
+function setup() {
+  createCanvas(800,400);
+  fixed=createSprite(400, 200, 50, 50);
+  moving=createSprite(100,100,50,50);
+  fixed.shapeColor="red"
+  moving.shapeColor="red"
+}
+
+function draw() {
+  background(255,255,255);
+  moving.x=mouseX
+  moving.y=mouseY  
+  if(moving.x-fixed.x<=moving.width/2+fixed.width/2 && fixed.x-moving.x<=moving.width/2+fixed.width/2 && moving.y-fixed.y<=moving.height/2+fixed.height/2 && fixed.y-moving.y<=moving.height/2+fixed.height/2){
+    fixed.shapeColor="green"
+    moving.shapeColor="green" 
+  }
+  else{
+    fixed.shapeColor="red"
+    moving.shapeColor="red"
+  }
+  drawSprites();
+}
